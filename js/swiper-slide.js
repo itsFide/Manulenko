4e0a2e8bf57b2e895e768da37859dab1cfc44eea
sergey-murzak <murzak301@gmail.com>
2021-03-17 03:15:05 +0600

    finish

---
+++ js/swiper-slide.js
@@ -0,0 +1,55 @@
+new Swiper('.testimonials',{
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    }
+});
+new Swiper('.volounteer',{
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    }
+});
+new Swiper('.road',{
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    }
+});
+new Swiper('.photo-action',{
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    },
+    // breakpoints:{
+    //     320:{
+
+    //     }
+    // }
+});
+new Swiper('.creation-work',{
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    }
+});
